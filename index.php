43280aeb3ce43ecec83132bcb5b8aa06742d9bf7
yeonhyuck <yeonhyuck@gmail.com>
2024-09-23 19:58:49 +0900

    코드정리
    불필요한 코드 삭제 정리, 인증키 삭제, 코드 리팩토링

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include "config.php";
+?>
 <html>
     <head>
     <link rel="stylesheet" href="index.css">
@@ -5,43 +8,19 @@
     <body>
         <div>
             <ul>
-                <li class="left"><a class="button" href="./subway_info.php?line=1호선">1호선</a></li>
-                <li class="right"><a class="button" href="./subway_info.php?line=2호선">2호선</a></li>
-            </ul>
-            <div class="clear"></div>
-            <ul>
-                <li class="left"><a class="button" href="./subway_info.php?line=3호선">3호선</a></li>
-                <li class="right"><a class="button" href="./subway_info.php?line=4호선">4호선</a></li>
-            </ul>
-            <div class="clear"></div>
-            <ul>
-                <li class="left"><a class="button" href="./subway_info.php?line=5호선">5호선</a></li>
-                <li class="right"><a class="button" href="./subway_info.php?line=6호선">6호선</a></li>
-            </ul>
-            <div class="clear"></div>
-            <ul>
-                <li class="left"><a class="button" href="./subway_info.php?line=7호선">7호선</a></li>
-                <li class="right"><a class="button" href="./subway_info.php?line=8호선">8호선</a></li>
-            </ul>
-            <div class="clear"></div>
-            <ul>
-                <li class="left"><a class="button" href="./subway_info.php?line=9호선">9호선</a></li>
-                <li class="right"><a class="button" href="./subway_info.php?line=경의중앙선">경의중앙선</a></li>
-            </ul>
-            <div class="clear"></div>
-            <ul>
-                <li class="left"><a class="button" href="./subway_info.php?line=공항철도">공항철도</a></li>
-                <li class="right"><a class="button" href="./subway_info.php?line=경춘선">경춘선</a></li>
-            </ul>
-            <div class="clear"></div>
-            <ul>
-                <li class="left"><a class="button" href="./subway_info.php?line=신분당선">신분당선</a></li>
-                <li class="right"><a class="button" href="./subway_info.php?line=수인분당선">수인분당선</a></li>
-            </ul>
-            <div class="clear"></div>
-            <ul>
-                <li class="left" ><a class="button" href="./subway_info.php?line=경강선">경강선</a></li>
-                <li class="right"><a class="button" href="./subway_info.php?line=GTX-A">GTX-A</a></li>
+                <?php
+                    $sql = "SELECT * FROM `subway`";
+                    $result = $conn->query($sql);
+                    while($row = $result->fetch_assoc()) {
+                        $dnlcl = "right";
+                        if($row['idx'] % 2 != 0) {
+                            $dnlcl = "left";
+                        } 
+                        ?>
+                        <li class="<?php echo $dnlcl ?>"><a class="button" href="./subway_info.php?line=<?php echo $row['idx']; ?>"><?php echo $row['line']; ?></a></li>
+                        <?php
+                    }
+                ?>
             </ul>
             <div class="clear"></div>
         </div>
--- subway_info.php
+++ subway_info.php
@@ -1,42 +1,25 @@
-<!DOCTYPE html>
-<html>
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "dusgur3608!";
-$dbname = "subway";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include "config.php";
 $line = $_GET['line'];
-// include "json.php";
-// include "GTX.php";
-?>
+$sql = "SELECT `name` FROM `lines` WHERE `subway_idx` = '$line' ";
+$result = $conn->query($sql);
+$row = $result->fetch_array();
+$line = $row['name'];
 
-<?php
 // cURL 세션 초기화
 $ch = curl_init();
-
-// 요청할 URL 설정 (이 URL은 JSON 데이터를 반환하는 URL이어야 합니다)
-$url = "http://swopenAPI.seoul.go.kr/api/subway/73467a4c6c79656f393754426c4a6b/json/realtimePosition/0/200/" . $line;
-
+// 요청할 URL 설정 
+$url = "http://swopenAPI.seoul.go.kr/api/subway/인증키/json/realtimePosition/0/200/" . $line;
 // URL 설정
 curl_setopt($ch, CURLOPT_URL, $url);
-
 // 반환값을 문자열로 설정
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 // cURL 실행 및 결과 저장
 $response = curl_exec($ch);
-
 // HTTP 응답 코드 확인
 $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
 // cURL 세션 닫기
 curl_close($ch);
-
 // HTTP 응답 코드가 200일 경우 JSON 데이터 출력
 if ($http_code == 200) {
     // JSON 문자열을 PHP 배열로 변환
@@ -44,26 +27,26 @@ if ($http_code == 200) {
     // 데이터 출력 (여기서는 예시로 첫 번째 열차 정보를 출력)
 } else {
     echo "HTTP 요청 실패, 응답 코드: $http_code\n";
+    exit;
 }
 
-$up = [];
+$subway = array(
+    "down" => array(),
+    "up" => array()
+);
 
-$down = [];
 
 foreach($data['realtimePositionList'] as $key => $value){
     //상행=나머지 하행=1 구분
     if ($value['updnLine'] == '1') {
-        array_push($down, $value);
+        $subway['down'][] = $value; 
+        
     } else {
-        array_push($up, $value);
+        $subway['up'][] = $value;
+
     }    
-}    
-
-
+} 
 ?>
-
-
-
 <head>
     <title><?php echo $line ?>노선도</title>
     <link rel="stylesheet" href="linestyle.css?v=<?php echo time();?>">
@@ -80,74 +63,61 @@ foreach($data['realtimePositionList'] as $key => $value){
         </a>
     </div>
     <div class="refresh_btn">
-            <div class="container"> 
+        <div class="container"> 
             <div class="btn" onClick="window.location.reload()">
-                
                 <span>새로고침</span>
                 <div class="dot"></div>
             </div>
-            </div>
+        </div>
     </div>
     <div class="line-title"><?php echo $line ?></div>
     <div class="line_main">
-        <ul id="down">
-        <div class="updn"> 하행 </div>
+
+        <?php
+        
+        foreach($subway as $k => $i) {
+            if($k == "up") {
+                $updown = "상행";
+                $tri = "▼";
+            }
+            else{
+                $updown = "하행";
+                $tri = "▲";  
+            }
+            
+        ?>
+        <ul id=<?php echo $k; ?>>
+            <div class="updn"> <?php echo $updown; ?></div>
             <?php
                 $sql = "SELECT * FROM `lines` WHERE `name` = '$line'";
                 $result = $conn->query($sql);
                 while($row = $result->fetch_assoc()) {
-                    echo "<li data-station_name='".$row['station_name']."'>";
-                    echo "<span class='line2';>";
-                    echo "<span class='box';>▲</span>";
-                    foreach ($down as $key => $value) {
+                    echo "<li data-station_name={$row['station_name']}>";
+                    if ($k == "up") {
+                        echo "<span class='station'>{$row['station_name']}</span>";
+                    }
+                    echo "<span class='line_{$k}'>";
+                    echo "<span class='box_{$k}';>{$tri}</span>";
+                    foreach ($subway[$k] as $key => $value) {
                         if ( $row['station_name'] == $value['statnNm']) {
                             $top = $value['trainSttus'] == '1' ? 50 : 90;
-                            echo "<span class='img' style='top:.$top.px;'>";
+                            echo "<span class='img' style='top:{$top}px;'>";
                             echo "<img src='pngegg.png' alt='subway' height='30' width='30'/>";
                             echo "</span>";
                         } 
                     }
                     if (strpos($row['code2'], 'C') !== false) {
-                        echo "<div class='cut'></div>";
+                        if ($k == "down") {
+                            echo "<div class='cut'></div>";
+                        }
                     }
                     echo "</span>";
                     echo "</li>";
                 }
             ?>
         </ul>
-        <ul id="up">
-        <div class="updn"> 상행 </div>
-            <?php
-            $sql = "SELECT * FROM `lines` WHERE `name` = '$line'";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    $station_name = $row['station_name'];
-                    echo "<li>";
-                    echo "<span class='station'>" . $row["station_name"] ."</span>";
-                    echo "<span class='line'>";
-                    echo "<span class='box2'>▼</span>";
-                    foreach ($up as $key => $value) {
-                        if ( $station_name == $value['statnNm']) {
-                            if ($value['trainSttus'] == '1') {
-                                $top = 50;
-                            }else{
-                                $top = 90;
-                            }
-                            echo "<span class='img' style='top:.$top.px;'>";
-                            echo "<img src='pngegg.png' alt='subway' height='30' width='30'/>";
-                            echo "</span>";
-                        } 
-                    }
-
-                    echo "</span>";
-                    echo "</li>";
-                }
-            } else {
-                echo "0 results";
-            }
-            ?>
-        </ul>
+        <?php  } ?>
+    
             
     </div>
 </body>
